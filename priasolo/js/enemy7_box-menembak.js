ab54d508613f9ae1d6dd659a88e0bef0a3a0f56f
bangyete17 <bangyete17@gmail.com>
2026-06-08 22:55:47 +0800

    Create enemy7_box-menembak.js

---
+++ priasolo/js/enemy7_box-menembak.js
@@ -0,0 +1,93 @@
+// ================================
+// ENEMY 7- PELURU
+// ================================
+
+var peluru7List = [];
+
+function enemy7Tembak(musuh){
+
+    
+    if(!game.aktif) return;
+
+    var peluru = {};
+
+    // spawn dari mulut senjata
+    if(musuh.skalaX == 1){
+        peluru.x = musuh.x + 20;
+        peluru.sx = 10
+    }else{
+        peluru.x = musuh.x - 20;
+        peluru.sx = -10
+    }
+
+    peluru.y = musuh.y - 10;
+// kecepatan vertikal awal
+peluru.sy = 0;
+    
+    peluru.sprite = setSprite(
+        dataGambar.peluru7,
+        16,
+        16
+    );
+
+    peluru7List.push(peluru);
+}
+
+function updatePeluru7(){
+
+    if(!game.aktif) return;
+
+    for(
+        var i = peluru7List.length - 1;
+        i >= 0;
+        i--
+    ){
+
+        var p = peluru7List[i];
+
+       // gerak lurus
+p.x += p.sx;
+
+// gravitasi peluru
+p.sy += 0.04
+p.y += p.sy;
+
+// gambar peluru langsung dari posisi yang disimpan
+p.sprite.x = Math.round(p.x);
+p.sprite.y = Math.round(p.y);
+
+        // tampilkan animasi peluru
+        loopSprite(p.sprite);
+
+        // tabrak hero
+        if(
+            jarak(
+                game.karakter.x,
+                game.karakter.y,
+                p.x,
+                p.y
+            ) < 20
+        ){
+
+            heroDead();
+
+            peluru7List.splice(i,1);
+
+            continue;
+        }
+
+        // keluar layar
+        if(
+            p.x < -300 ||
+            p.x > game.lebar + 300
+        ){
+
+            peluru7List.splice(i,1);
+        }
+    }
+}
+
+function resetEnemy7(){
+
+    peluru7List = [];
+}
